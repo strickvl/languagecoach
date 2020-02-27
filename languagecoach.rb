fb3299d442c43ab6d2fd5ecbda71447ac8698b87
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2020-02-27 12:55:16 +0500

    add new routes

--- languagecoach.rb
+++ languagecoach.rb
@@ -2,3 +2,32 @@ require "sinatra"
 require "sinatra/reloader"
 require "sinatra/content_for"
 require "tilt/erubis"
+
+configure do
+  set :erb, :escape_html => true
+end
+
+get "/" do
+  @title = "Home"
+  erb :front_page, layout: :layout
+end
+
+get "/coachbot" do
+  erb :coachbot, layout: :layout
+end
+
+get "/masterarabic" do
+  erb :masterarabic, layout: :layout
+end
+
+get "/coaching" do
+  erb :coaching, layout: :layout
+end
+
+get "/podcast" do
+  erb :podcast, layout: :layout
+end
+
+get "about" do
+  erb :about, layout: :layout
+end
