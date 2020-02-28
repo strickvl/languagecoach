071bd0875356f2646351d08a688e7678b3a4f5f7
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2020-02-28 12:41:12 +0500

    fix routes to coachbot app

--- languagecoach.rb
+++ languagecoach.rb
@@ -2,6 +2,7 @@ require "sinatra"
 require "sinatra/reloader"
 require "sinatra/content_for"
 require "tilt/erubis"
+require 'csv'
 
 configure do
   set :erb, :escape_html => true
@@ -43,26 +44,45 @@ get "/howmuchtime" do
 end
 
 get "/fivemintask" do
+  task = CSV.read("./public/tasks5mins.csv").shuffle.first
+  @task_name = task[0]
+  @task_extra_information = task[1]
   @title = "Five Minute Task"
   erb :fivemintask, layout: :layout
 end
 
 get "/tenmintask" do
+  task = CSV.read("./public/tasks10mins.csv").shuffle.first
+  @task_name = task[0]
+  @task_extra_information = task[1]
+
   @title = "Ten Minute Task"
   erb :tenmintask, layout: :layout
 end
 
 get "/twentymintask" do
+  task = CSV.read("./public/tasks20mins.csv").shuffle.first
+  @task_name = task[0]
+  @task_extra_information = task[1]
+
   @title = "Twenty Minute Task"
   erb :twentymintask, layout: :layout
 end
 
 get "/thirtymintask" do
+  task = CSV.read("./public/tasks30mins.csv").shuffle.first
+  @task_name = task[0]
+  @task_extra_information = task[1]
+
   @title = "Thirty Minute Task"
   erb :thirtymintask, layout: :layout
 end
 
 get "/sixtymintask" do
+  task = CSV.read("./public/tasks60mins.csv").shuffle.first
+  @task_name = task[0]
+  @task_extra_information = task[1]
+
   @title = "Sixty Minute Task"
   erb :sixtymintask, layout: :layout
 end
